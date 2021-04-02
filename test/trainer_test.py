011a0eb6b66099773cc22c5c8f1bf60b48189013
mgiurca <mgiurca.mail@gmail.com>
2021-04-03 00:03:22 +0300

    Logger without auto hp removed

--- test/trainer_test.py
+++ test/trainer_test.py
@@ -1,12 +1,17 @@
+from pytorch_lightning.loggers import TensorBoardLogger
 from pytorch_lightning.trainer import Trainer
 
-from src.utils.Datamodule import CaliforniaFloodDataModule
 from src.neuralNet.NeuralNet import NeuralNetwork
+from src.utils.Datamodule import CaliforniaFloodDataModule
 
 if __name__ == '__main__':
-    datamodule = CaliforniaFloodDataModule(batch_size=8)
+    datamodule = CaliforniaFloodDataModule()
     model = NeuralNetwork()
 
-    trainer = Trainer(gpus=1, max_epochs=100)
+    trainer = Trainer(gpus=1, max_epochs=100,
+                      logger=TensorBoardLogger(save_dir='lightning_logs', default_hp_metric=False),
+                      # auto_lr_find=True,
+                      # auto_scale_batch_size=True
+                      )
 
     trainer.fit(model, datamodule=datamodule)
