723fb60bb689191cb99528224e23a1b2c748118c
Madalin-Stefan Giurca <mgiurca.mail@gmail.com>
2021-05-09 00:28:54 +0300

    added test Dataset class

--- src/utils/Dataset.py
+++ src/utils/Dataset.py
@@ -16,3 +16,20 @@ class CaliforniaFloodDataset(Dataset):
 
     def __len__(self):
         return self.no_samples
+
+
+class CaliforniaTestDataset(Dataset):
+    def __init__(self, data: dict):
+        assert data['landsat'].shape[0] == data['sentinel'].shape[0] == data['roi'].shape[0]
+
+        self.ground_truth = data['roi']
+        self.landsat_patches = data['landsat']
+        self.sentinel_patches = data['sentinel']
+
+        self.no_samples = data['landsat'].shape[0]
+
+    def __getitem__(self, item):
+        return self.landsat_patches[item], self.sentinel_patches[item], self.ground_truth[item]
+
+    def __len__(self):
+        return self.no_samples
