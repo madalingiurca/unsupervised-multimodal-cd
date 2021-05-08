ea1a3ba37c442e300acf9a60323392e4b4c12cf4
Madalin-Stefan Giurca <mgiurca.mail@gmail.com>
2021-05-09 00:31:13 +0300

    test script added and Datamodule fixes

---
+++ src/test_model.py
@@ -0,0 +1,104 @@
+import numpy as np
+import sklearn
+import torch
+from matplotlib import pyplot as plt
+from patchify import unpatchify
+from skimage.filters import threshold_otsu
+from sklearn.cluster import KMeans
+from tqdm import tqdm
+
+from src.neuralNetwork.ace_net import AceNet
+from src.utils.CaliforniaFloodDataModule import CaliforniaFloodDataModule
+
+if __name__ == '__main__':
+
+    model = AceNet()
+    model = model.load_from_checkpoint(r"checkpoints/epoch=249-step=21999.ckpt")
+    print(model)
+    model.eval()
+
+    test_patch_size = 250
+
+    datamodule = CaliforniaFloodDataModule(patch_size=test_patch_size, window_step=test_patch_size, batch_size=1)
+    mask = datamodule.setup()["roi"]
+    dataloader = datamodule.test_dataloader()
+    # diff_image = np.zeros((3500, 2000))
+    patches = []
+
+    for step, item in enumerate(tqdm(dataloader)):
+        x, y, ground_truth = item
+        if step % 30 == 0:
+            plt.figure(), plt.imshow(y[0]), plt.show()
+            plt.figure(), plt.imshow((x[0, :, :, [3, 2, 1]] + 1) / (2 * x.max()))
+
+        x = x.permute(0, 3, 1, 2)
+        y = y.permute(0, 3, 1, 2)
+
+        x_hat, y_hat = model(x, y)
+
+        x_hat = x_hat.permute(0, 2, 3, 1).detach().numpy()[0]
+        y_hat = y_hat.permute(0, 2, 3, 1).detach().numpy()[0]
+
+        if step % 30 == 0:
+            plt.figure(), plt.imshow(y_hat), plt.show()
+            plt.figure(), plt.imshow((x_hat[:, :, [3, 2, 1]] + 1) / (2 * x.max()))
+
+        x = x.permute(0, 2, 3, 1)[0]
+        y = y.permute(0, 2, 3, 1)[0]
+        # out_image_t1 = np.sum((x - x_hat) ** 2, 0)
+
+        # Compute 2nd norm of image diff
+        out_image_t1 = torch.linalg.norm((x - x_hat), ord=2, axis=-1)
+        # Clip the value out of range 3*std to 3*std
+        out_image_t1[out_image_t1 > torch.mean(out_image_t1) + 3 * torch.std(out_image_t1)] = torch.mean(
+            out_image_t1) + 3 * torch.std(out_image_t1)
+        # Normalize
+        out_image_t1 = out_image_t1 / out_image_t1.max()
+
+        out_image_t2 = torch.linalg.norm((y - y_hat), ord=2, axis=-1)
+        # out_image_t2 = torch.sum((y[0] - y_hat[0]) ** 2, 0).detach().numpy()
+        out_image_t2[out_image_t2 > torch.mean(out_image_t2) + 3 * torch.std(out_image_t2)] = torch.mean(
+            out_image_t2) + 3 * torch.std(out_image_t2)
+        out_image_t2 = out_image_t2 / out_image_t2.max()
+
+        diff_patch = (out_image_t1 + out_image_t2) / 2.0
+        if step % 30 == 0:
+            plt.figure(), plt.imshow(diff_patch), plt.show()
+            plt.figure(), plt.imshow(ground_truth[0]), plt.show()
+
+        patches.append(diff_patch.detach().numpy())
+
+    diff_image = np.array(patches)
+    diff_image = unpatchify(np.array(diff_image).reshape(
+        (3500 // test_patch_size, 2000 // test_patch_size, test_patch_size, test_patch_size)), (3500, 2000))
+    # diff_image = np.reshape(diff_image, (3500, 2000))
+    plt.figure(), plt.imshow(diff_image), plt.show()
+
+    AUC = sklearn.metrics.roc_auc_score(mask.ravel(), diff_image.ravel())
+
+
+    # np.save('diff_image.npy', diff_image)
+
+    # k-means clustering
+    k_m = KMeans(n_clusters=2, n_init=4)
+    k_m.fit(diff_image.reshape((-1, 1)))
+    # Get the coordinates of the clusters centres as a 1D array
+    values = k_m.cluster_centers_.squeeze()
+    # Get the label of each point
+    labels = k_m.labels_
+    img_segm = np.choose(labels, values)
+    img_segm.shape = diff_image.shape
+    plt.imshow(img_segm, cmap='binary'), plt.show()
+
+    OA = sklearn.metrics.accuracy_score(mask.flatten(), img_segm.flatten())
+
+    img_segm.shape = diff_image.shape
+    img_segm = diff_image < 0.1
+    plt.figure(), plt.imshow(img_segm), plt.show()
+
+    threshold = threshold_otsu(diff_image)
+    img_segm = diff_image < threshold
+
+    OA = sklearn.metrics.accuracy_score(mask.flatten(), img_segm.flatten())
+
+    plt.figure(), plt.imshow(img_segm, cmap='binary'), plt.show()
--- src/utils/CaliforniaFloodDataModule.py
+++ src/utils/CaliforniaFloodDataModule.py
@@ -1,11 +1,13 @@
 import os
+from typing import Union, List
 
 from pytorch_lightning import LightningDataModule
 from scipy.io import loadmat
 from torch.utils.data import DataLoader
+from skimage.util import view_as_blocks
 
 from src.utils import prior_computation
-from src.utils.Dataset import CaliforniaFloodDataset
+from src.utils.Dataset import CaliforniaFloodDataset, CaliforniaTestDataset
 
 
 class CaliforniaFloodDataModule(LightningDataModule):
@@ -13,6 +15,7 @@ class CaliforniaFloodDataModule(LightningDataModule):
                  batch_size=16, patch_size=20, window_step=10):
         super().__init__()
         self.trainDataset = None
+        self.testDataset = None
         self.batch_size = batch_size
         self.data_path = data_path
         self.patch_size = patch_size
@@ -20,7 +23,6 @@ class CaliforniaFloodDataModule(LightningDataModule):
         self.data = {}
 
     def prepare_data(self, *args, **kwargs):
-
         if not os.path.exists('resources/alpha_prior_image.png'):
             prior_computation.save_priorinfo_image(patch_size=self.patch_size, window_step=self.window_step)
 
@@ -28,18 +30,31 @@ class CaliforniaFloodDataModule(LightningDataModule):
         matFile = loadmat(self.data_path)
         data = dict()
 
-        data['landsat'] = matFile['t1_L8_clipped'].view()
-        data['landsat'] = data['landsat'].reshape((-1, self.patch_size, self.patch_size, 11))
-        data['sentinel'] = matFile['logt2_clipped'].view()
-        data['sentinel'] = data['sentinel'].view().reshape((-1, self.patch_size, self.patch_size, 3))
+        data['landsat'] = matFile['t1_L8_clipped']
+        # data['landsat'] = data['landsat'].reshape((-1, self.patch_size, self.patch_size, 11))
+        data['landsat'] = view_as_blocks(data['landsat'], (self.patch_size, self.patch_size, 11)).reshape((-1, self.patch_size, self.patch_size, 11))
+        data['sentinel'] = matFile['logt2_clipped']
+        # data['sentinel'] = data['sentinel'].view().reshape((-1, self.patch_size, self.patch_size, 3))
+        data['sentinel'] = view_as_blocks(data['sentinel'], (self.patch_size, self.patch_size, 3)).reshape((-1, self.patch_size, self.patch_size, 3))
 
         prior_information_image = loadmat(r'resources/alpha_prior_test.mat')['prior']
         prior_information_image = prior_information_image / prior_information_image.max()
 
-        data['prior_info'] = prior_information_image.view()
-        data['prior_info'] = data['prior_info'].view().reshape((-1, self.patch_size, self.patch_size))
-
+        data['prior_info'] = prior_information_image
+        # data['prior_info'] = data['prior_info'].view().reshape((-1, self.patch_size, self.patch_size))
+        data['prior_info'] = view_as_blocks(data['prior_info'], (self.patch_size, self.patch_size)).reshape((-1, self.patch_size, self.patch_size))
         self.trainDataset = CaliforniaFloodDataset(data)
+
+        data['roi'] = matFile['ROI']
+        # data['roi'] = data['roi'].view().reshape((-1, self.patch_size, self.patch_size))
+        data['roi'] = view_as_blocks(data['roi'], (self.patch_size, self.patch_size)).reshape((-1, self.patch_size, self.patch_size))
+
+        self.testDataset = CaliforniaTestDataset(data)
+
+        return data
 
     def train_dataloader(self, *args, **kwargs) -> DataLoader:
         return DataLoader(self.trainDataset, batch_size=self.batch_size, shuffle=True)
+
+    def test_dataloader(self) -> Union[DataLoader, List[DataLoader]]:
+        return DataLoader(self.testDataset, batch_size=self.batch_size)
